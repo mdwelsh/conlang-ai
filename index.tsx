974da34491fdd366172f4e445f6e47a2b8ba78b7
Matt Welsh <matt@fixie.ai>
2023-09-03 15:08:55 -0700

    Working on this.

--- index.tsx
+++ index.tsx
@@ -1,22 +1,37 @@
-/**
- * This is a simple "Hello World" example for AI.JSX, a library for building
- * AI-powered applications using JavaScript and JSX. For more details, check out:
- *   https://ai-jsx.com/
- *
- * Run this example with:
- *   $ export OPENAI_API_KEY=<Your OpenAI API key>
- *   $ npm install
- *   $ npm start
- */
-
 import * as AI from "ai-jsx";
-import { ChatCompletion, UserMessage } from "ai-jsx/core/completion";
+import {
+  ChatCompletion,
+  SystemMessage,
+  UserMessage,
+} from "ai-jsx/core/completion";
+import path from "path";
+import fs from "fs";
+import terminal from "terminal-kit";
+const { terminal: term } = terminal;
 
 function App() {
+  const glyphs: Map<string, string> = new Map();
+  const allGlyphs = fs.readdirSync("./glyphs");
+  for (const glyph of allGlyphs) {
+    term("Reading ").green(glyph).green("...\n");
+    glyphs.set(glyph, fs.readFileSync(path.join("./glyphs", glyph), "utf-8"));
+  }
+  const glyphPrompt: string[] = [];
+  glyphs.forEach((glyph, word) => glyphPrompt.push(`The glyph for "${word.split(".")[0]}" is: ${glyph}{'\n'}`));
+
   return (
     <ChatCompletion>
+      <SystemMessage>
+        You are an AI assistant that generates glyphs for a constructed
+        language. You will be given examples of words in the langugae, along
+        with glyphs in SVG format. Your job is to generate new glyphs for new
+        words.
+      </SystemMessage>
       <UserMessage>
-        Generate a Shakespearean sonnet about large language models.
+        You are generating glyphs for a constructed language called Derpytext.
+        The glyphs are represented in SVG format.{"\n"}
+        {glyphPrompt.join("\n")}
+        Please generate a glyph for the word "broom".
       </UserMessage>
     </ChatCompletion>
   );
